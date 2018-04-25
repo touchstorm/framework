c5e80fcf34cff05a14c190d83ee5c3f1e895c4f1
seomike <seomike@gmail.com>
2018-04-24 19:34:54 -0500

    Running task constructor added.

--- src/Chronos/Tasks/Running.php
+++ src/Chronos/Tasks/Running.php
@@ -2,7 +2,29 @@
 
 namespace Chronos\Tasks;
 
+use Chronos\Tasks\Exceptions\TaskCollectionException;
+
 class Running extends Task
 {
+    /**
+     * Running constructor.
+     * @param $name
+     * @param $arguments
+     * @throws TaskCollectionException
+     */
+    public function __construct($name, $arguments = null)
+    {
+        if (is_null($arguments)) {
+            throw new TaskCollectionException('Task cannot be initiated. Missing arguments' . 422);
+        }
 
+        // If arguments come over as an array
+        if (is_array($arguments)) {
+            parent::__construct($name, $arguments);
+            return;
+        }
+
+        // Default assumes a string
+        parent::__construct($name, ['uses' => $arguments]);
+    }
 }
